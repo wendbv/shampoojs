37b01ccf482892ad6fb536a8c2ccf017d903d303
Martijn Arts <arts.martijn@gmail.com>
2015-12-18 13:20:51 +0100

    Write initial code

--- spec/shampoo.spec.js
+++ spec/shampoo.spec.js
@@ -1,6 +1,71 @@
 'use strict';
 
-var sh = require('../bin/shampoo');
+var rewire = require('rewire');
+
+var sh = rewire('../bin/shampoo');
+var Shampoo = sh.Shampoo;
+
+var sendSpy = jasmine.createSpy('WS.Send');
+var closeSpy = jasmine.createSpy('WS.Close');
+var wsSpy = jasmine.createSpy('WebSocket').and.callFake(
+    function() {
+        this.onopen = undefined;
+        this.onmessage = undefined;
+        this.onerror = undefined;
+        this.onclose = undefined;
+        this.send = sendSpy;
+        this.close = closeSpy;
+
+        return this;
+    });
+sh.__set__('WebSocket', wsSpy);
 
 describe('Shampoo', () => {
+    beforeEach(() => {
+        this.s = new Shampoo('foo');
+    });
+    afterEach(() => {
+        this.s.close();
+        this.s = undefined;
+    });
+
+    it('should instantiate right', () => {
+        expect(wsSpy).toHaveBeenCalledWith('foo', 'shampoo');
+    });
+
+    it('should open correctly', () => {
+        this.s.socket.onopen();
+        expect(this.s.ready).toBe(true);
+    });
+
+    it('should allow me to call a method', () => {
+        spyOn(this.s, 'sendMessage');
+        this.s.call('foo', {});
+
+        expect(this.s.sendMessage).toHaveBeenCalled();
+    });
+
+    it('should keep its promises', (done) => {
+        this.s.call('foo', {})
+            .then(done, () => {});
+
+        this.s.socket.onmessage({data: {
+            status: 200,
+            message: 'ok',
+            response_data: {},
+            request_index_number: 1,
+        }});
+    });
+
+    it('should keep its promises even if something goes wrong', (done) => {
+        this.s.call('foo', {})
+            .then(() => {}, done);
+
+        this.s.socket.onmessage({data: {
+            status: 501,
+            message: 'Server Error',
+            response_data: {},
+            request_index_number: 1,
+        }});
+    });
 });
--- src/shampoo.ts
+++ src/shampoo.ts
@@ -1,1 +1,104 @@
-class Shampoo {}
+/// <reference path="typings/promise.d.ts" />
+
+interface Response {
+    status: number;
+    message: string;
+    response_data: any;
+    request_index_number: number;
+}
+interface ResponseEvent extends Event {
+    data: Response;
+}
+
+interface Request<T> {
+    method: string;
+    request_data: T;
+    index_number: number;
+}
+
+interface PromiseObj<R> {
+    resolve: (data: Response) => void,
+    reject: (data: Response) => void,
+    promise: Promise<R>
+}
+interface MessageMap {
+    [index: number]: PromiseObj<any>;
+}
+
+
+export class Shampoo {
+    index: number = 0;
+
+    socket: WebSocket;
+    ready: boolean = false;
+
+    messageMap: MessageMap = {};
+
+    constructor(uri: string) {
+        this.socket = new WebSocket(uri, 'shampoo');
+
+        this.socket.onopen = this.onReady.bind(this);
+        this.socket.onmessage = this.onMessage.bind(this);
+        this.socket.onerror = this.onError.bind(this);
+        this.socket.onclose = this.onClose.bind(this);
+    }
+
+    private onReady(e: Event) {
+        this.ready = true;
+    }
+
+    private onMessage(e: ResponseEvent) {
+        let p = this.messageMap[e.data.request_index_number];
+        if(e.data.status >= 200 && e.data.status <= 299) {
+            p.resolve(e.data.response_data);
+        } else {
+            p.reject(e.data.response_data);
+        }
+    }
+
+    private onError(e: Event) {
+    }
+
+    private onClose(e: Event) {
+        this.ready = false;
+    }
+
+    close() {
+        this.socket.close();
+    }
+
+    call<T>(method: string, data: T): Promise<T> {
+        this.index += 1;
+
+        let message: Request<T> = {
+            method: method,
+            request_data: data,
+            index_number: this.index,
+        };
+
+        let promiseObj: PromiseObj<T> = {
+            resolve: (data: Response) => {},
+            reject: (data: Response) => {},
+            promise: undefined,
+        };
+
+        let promise = promiseObj.promise = new Promise<T>((resolve, reject) => {
+            this.sendMessage(message);
+
+            promiseObj.resolve = (data: Response) => {
+                resolve(<T>data.response_data);
+            };
+            promiseObj.reject = (data: Response) => {
+                reject(<T>data.response_data);
+            };
+        });
+
+        this.messageMap[this.index] = promiseObj;
+
+        return promise;
+    }
+
+    private sendMessage<T>(data: Request<T>): void {
+        this.socket.send(JSON.stringify(data));
+    }
+}
